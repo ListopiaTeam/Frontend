862d801ef7e75f181a728573096384186ca0bcb4
MountainSpiritq <greenbull122@gmail.com>
2025-01-24 10:22:19 +0100

    firebase basics

---
+++ utility/crudUtility.js
@@ -0,0 +1,19 @@
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    DocumentSnapshot,
+    getDoc,
+    onSnapshot,
+    orderBy,
+    query,
+    serverTimestamp,
+    updateDoc,
+    where,
+  } from "firebase/firestore";
+  import { db } from "./firebaseApp";
+
+  //oh god
+
+  
